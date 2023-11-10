b079080fceae9067b108b9dd1ed4e95480aff4db
Luiz Gustavo <luizgusqueiroz@gmail.com>
2023-11-10 16:07:12 +0000

    Final commit atividade2 questao_02

--- atividade2/questao_02.py
+++ atividade2/questao_02.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # Abrindo imagem
 
-image_path = 'frutas.jpg'
+image_path = 'imagem3.jpg'
 image = cv2.imread(image_path)
 
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -12,13 +12,13 @@ plt.axis('off')
 
 
 # Filtro da média
-blurred_image_mean = cv2.blur(image, (5,5))
+blurred_image_mean = cv2.blur(image_rgb, (5,5))
 
 # Filtro da mediana
-...
+blurred_image_medi = cv2.medianBlur(image_rgb, 5)
 
 # Filtro Gaussiano
-blurred_image_gaus = cv2.GaussianBlur(image, (5,5), 0)
+blurred_image_gaus = cv2.GaussianBlur(image_rgb, (5,5), 0)
 
 
 
@@ -34,7 +34,7 @@ plt.imshow(blurred_image_mean)
 plt.title('Filtro da média')
 
 plt.subplot(2, 2, 3)
-#plt.imshow()
+plt.imshow(blurred_image_medi)
 plt.title('Filtro da mediana')
 
 plt.subplot(2, 2, 4)
